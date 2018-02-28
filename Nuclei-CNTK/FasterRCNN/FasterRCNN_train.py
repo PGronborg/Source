c974412329588c5bf7d3c6986240def2519a9c77
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-02-28 16:00:00 +0100

    Small changes

--- Nuclei-CNTK/FasterRCNN/FasterRCNN_train.py
+++ Nuclei-CNTK/FasterRCNN/FasterRCNN_train.py
@@ -563,14 +563,14 @@ def train_model(image_input, roi_input, dims_input, loss, pred_error,
     else:
         input_map[od_minibatch_source.dims_si] = dims_input
 
-    progress_printer = ProgressPrinter(tag='Training', num_epochs=epochs_to_train, gen_heartbeat=True)
+    progress_printer = ProgressPrinter(freq = None,tag='Training', num_epochs=epochs_to_train, gen_heartbeat=True)
     for epoch in range(epochs_to_train):       # loop over epochs
         sample_count = 0
         while sample_count < cfg["DATA"].NUM_TRAIN_IMAGES:  # loop over minibatches in the epoch
             data = od_minibatch_source.next_minibatch(min(cfg.MB_SIZE, cfg["DATA"].NUM_TRAIN_IMAGES-sample_count), input_map=input_map)
             trainer.train_minibatch(data)                                    # update model with it
             sample_count += trainer.previous_minibatch_sample_count          # count samples processed so far
-            #progress_printer.update_with_trainer(trainer, with_metric=True)  # log progress
+            progress_printer.update_with_trainer(trainer, with_metric=True)  # log progress
             #if sample_count % 100 == 0:
                 #print("Processed {} samples".format(sample_count))
         progress_printer.epoch_summary(with_metric=True)
--- Nuclei-CNTK/FasterRCNN/runner.sh
+++ Nuclei-CNTK/FasterRCNN/runner.sh
@@ -6,4 +6,4 @@ CUDA_VISIBLE_DEVICES=7 python run_faster_rcnn_VGG16.py >> TextOutput1.txt
 clear
 CUDA_VISIBLE_DEVICES=7 python run_faster_rcnn_VGG16.py >> TextOutput1.txt
 
-mail -s 'Finished'  peter.gronborg.7@gmail. <<< "Done"
+mail -s 'Finished'  peter.gronborg.7@gmail.com <<< "Done"
