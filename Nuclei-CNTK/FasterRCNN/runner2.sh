1c2ada0dd91719decfd4e051e3de8d9fe823b97c
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-03-15 11:51:37 +0100

    Rettelse af tab fejl

--- Nuclei-CNTK/FasterRCNN/runner2.sh
+++ Nuclei-CNTK/FasterRCNN/runner2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 mail -s 'Starting'  peter.gronborg.7@gmail.com <<< "Start"
 
-CUDA_VISIBLE_DEVICES=7 python run_faster_rcnn_VGG16.py > TextOutput1.txt
+CUDA_VISIBLE_DEVICES=0 python run_faster_rcnn_VGG16.py > TextOutput2.txt
 
 mail -s 'Finished'  peter.gronborg.7@gmail.com <<< "Done"
