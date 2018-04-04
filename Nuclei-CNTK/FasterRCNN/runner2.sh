275eae1eaaacb2c2629fa20b38dd835df6ec5894
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-04-04 08:55:31 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/runner2.sh
+++ Nuclei-CNTK/FasterRCNN/runner2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 mail -s 'Starting'  peter.gronborg.7@gmail.com <<< "Start High"
 
-time CUDA_VISIBLE_DEVICES=1 python run_faster_rcnn_VGG16.py HighLow 0.05 > High.txt 2> errHigh.txt
+time CUDA_VISIBLE_DEVICES=1 python run_faster_rcnn_VGG16.py High 0.05 > High.txt 2> errHigh.txt
 
 mail -s 'Finished'  peter.gronborg.7@gmail.com <<< "Done High"
