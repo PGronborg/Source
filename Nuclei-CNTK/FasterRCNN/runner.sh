ddfab9905e5c4adf89af244e6e603e9c1fbead8b
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-04-04 08:53:15 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/runner.sh
+++ Nuclei-CNTK/FasterRCNN/runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 mail -s 'Starting'  peter.gronborg.7@gmail.com <<< "Start Low"
 
-time CUDA_VISIBLE_DEVICES=0 python run_faster_rcnn_VGG16.py Low 0.0000005  > Low.txt 2> errLow.txt
+time CUDA_VISIBLE_DEVICES=0 python run_faster_rcnn_VGG16.py Low 0.0000005 > Low.txt 2> errLow.txt
 
 mail -s 'Finished'  peter.gronborg.7@gmail.com <<< "Done Low"
