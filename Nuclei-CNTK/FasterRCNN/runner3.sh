32107bc53698e0ca5479a1c5840a1412aa11b5c3
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-04-05 11:25:04 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/runner3.sh
+++ Nuclei-CNTK/FasterRCNN/runner3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 mail -s 'Starting'  peter.gronborg.7@gmail.com <<< "Start Tester1"
 
-time CUDA_VISIBLE_DEVICES=4 python run_faster_rcnn_VGG16.py Tester1 0.00005  > Tester1.txt 2> errTester1.txt
+time CUDA_VISIBLE_DEVICES=5 python run_faster_rcnn_VGG16.py Tester1 0.00005  > Tester1.txt 2> errTester1.txt
 
 mail -s 'Finished'  peter.gronborg.7@gmail.com <<< "Done Tester1"
