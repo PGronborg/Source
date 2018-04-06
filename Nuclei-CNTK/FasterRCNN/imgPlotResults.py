bb28836552c6f6e811a7602a878a20a790016d0e
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-04-06 13:31:20 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/imgPlotResults.py
+++ Nuclei-CNTK/FasterRCNN/imgPlotResults.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-imgFolder = "/Volumes/Visiopharm Student HDD/F18_Peter/Data/Ki67/test_256_20XX/"
-file = "/Volumes/Visiopharm Student HDD/F18_Peter/Data/Ki67/test_256_20X/"
+imgFolder = "/Users/Greenborg/Desktop/Experiments/Train/Correct/"#"/Volumes/Visiopharm Student HDD/F18_Peter/Data/Ki67/test_256_20XX/"
+file = "/Users/Greenborg/Desktop/Experiments/"#"/Volumes/Visiopharm Student HDD/F18_Peter/Data/Ki67/test_256_20X/"
 num = int(sys.argv[1])
 
 #imgVal = num % 100
 #folderVal = (num - imgVal) / 100
 #imgVal = imgVal - 1
 
-Floc = open(file+"testval_nuclei.txt","r")
-Froi = open(file+"testval_nuclei_roi.txt","r")
+Floc = open(file+"trainval_nuclei.txt","r")
+Froi = open(file+"trainval_nuclei_roi.txt","r")
 
 lines = Floc.readlines()
 lineLoc = lines[num]
@@ -31,8 +31,11 @@ roi[0:2] = []
 
 for i in range(int(len(roi)/5)):
     ind = int(i*5)
-    cv2.rectangle(img,(round(float(roi[ind])),round(float(roi[ind+1]))),(round(float(roi[ind+2])),round(float(roi[ind+3]))),(0,255,0),1)
-    
+    #cv2.rectangle(img,(round(float(roi[ind])),round(float(roi[ind+1]))),(round(float(roi[ind+2])),round(float(roi[ind+3]))),(0,255,0),1)
+    if int(round(float(roi[ind+4]))) == 1:
+        cv2.circle(img,(int(round(np.mean((float(roi[ind]),float(roi[ind+2]))))),int(round(np.mean((float(roi[ind+1]),float(roi[ind+3])))))),2,(255,0,0),-1)
+    else:
+        cv2.circle(img,(int(round(np.mean((float(roi[ind]),float(roi[ind+2]))))),int(round(np.mean((float(roi[ind+1]),float(roi[ind+3])))))),2,(0,255,0),-1)
 
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.show()
