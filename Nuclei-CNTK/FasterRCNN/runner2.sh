177a21fb6dd729ff9b323b816d88572cc1297814
Peter Grønborg <Greenborg@Peters-MacBook-Pro-5.local>
2018-04-11 10:49:08 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/runner2.sh
+++ Nuclei-CNTK/FasterRCNN/runner2.sh
@@ -5,18 +5,15 @@ time CUDA_VISIBLE_DEVICES=3 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0
 
 time CUDA_VISIBLE_DEVICES=4 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET1 1 1 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET1.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET1.txt &
 
-wait
+time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET1_5 1 1.5 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET1_5.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET1_5.txt &
 
-time CUDA_VISIBLE_DEVICES=3 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET1_5 1 1.5 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET1_5.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET1_5.txt &
+wait
 
 time CUDA_VISIBLE_DEVICES=4 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET2 1 2 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET2.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET2.txt &
 
-
-wait
-
 time CUDA_VISIBLE_DEVICES=3 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET5 1 5 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET5.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET5.txt &
 
-time CUDA_VISIBLE_DEVICES=4 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET10 1 10 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET10.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET10.txt &
+time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center0_5/SigRPN1/SigDET10 1 10 > Output/SigmaCenter/Center0_5/SigRPN1/OutputSigDET10.txt 2> Output/SigmaCenter/Center0_5/SigRPN1/errSigDET10.txt &
 
 
 wait
