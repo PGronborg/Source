c89b0488f8afdf6f7f72f4b619791e577842678e
Peter Grønborg <Greenborg@Peters-MBP-5.k-net.dk>
2018-04-18 22:24:42 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/RunTest.sh
+++ Nuclei-CNTK/FasterRCNN/RunTest.sh
@@ -1,13 +1,17 @@
 #!/bin/bash 
 mail -s 'Starting'  peter.gronborg.7@gmail.com <<< "Start testrun"
 
-time CUDA_VISIBLE_DEVICES=0 python3 run_faster_rcnn_VGG16.py Layers/Ref pool2 drop7 > Output/Layers/RefOut.txt 2> Output/Layers/RefErr.txt &
+#time CUDA_VISIBLE_DEVICES=0 python3 run_faster_rcnn_VGG16.py Layers/Ref pool2 drop7 > Output/Layers/RefOut.txt 2> Output/Layers/RefErr.txt &
 
-time CUDA_VISIBLE_DEVICES=1 python3 run_faster_rcnn_VGG16.py Layers/Start_Pool3 pool3 drop7 > Output/Layers/Start_Pool3Out.txt 2> Output/Layers/Start_Pool3Err.txt &
+#time CUDA_VISIBLE_DEVICES=1 python3 run_faster_rcnn_VGG16.py Layers/Start_Pool3 pool3 drop7 > Output/Layers/Start_Pool3Out.txt 2> Output/Layers/Start_Pool3Err.txt &
 
-time CUDA_VISIBLE_DEVICES=2 python3 run_faster_rcnn_VGG16.py Layers/Start_Pool4 pool4 drop7 > Output/Layers/Start_Pool4Out.txt 2> Output/Layers/Start_Pool4Err.txt &
+#time CUDA_VISIBLE_DEVICES=2 python3 run_faster_rcnn_VGG16.py Layers/Start_Pool4 pool4 drop7 > Output/Layers/Start_Pool4Out.txt 2> Output/Layers/Start_Pool4Err.txt &
 
-time CUDA_VISIBLE_DEVICES=3 python3 run_faster_rcnn_VGG16.py Layers/Last_Drop6 pool2 drop6 > Output/Layers/Last_Drop6Out.txt 2> Output/Layers/Last_Drop6Err.txt &
+time CUDA_VISIBLE_DEVICES=1 python3 run_faster_rcnn_VGG16.py Layers/Last_Drop6 pool2 drop6 > Output/Layers/Last_Drop6Out.txt 2> Output/Layers/Last_Drop6Err.txt &
+time CUDA_VISIBLE_DEVICES=2 python3 run_faster_rcnn_VGG16.py Layers/Last_fc6 pool2 fc6 > Output/Layers/Last_Fc6ut.txt 2> Output/Layers/Last_Fc6Err.txt &
+time CUDA_VISIBLE_DEVICES=3 python3 run_faster_rcnn_VGG16.py Layers/Last_relu6 pool2 relu6 > Output/Layers/Last_Relu6Out.txt 2> Output/Layers/Last_Relu6Err.txt &
+
+
 wait
 
 mail -s 'Finished'  peter.gronborg.7@gmail.com <<< "Done testrun"
