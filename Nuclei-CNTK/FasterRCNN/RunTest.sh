7589f045631a0cf20f2a92a0b151906a23b3e53b
Peter Grønborg <Greenborg@Peters-MBP-5.k-net.dk>
2018-05-02 15:25:57 +0200

    Rettelse

--- Nuclei-CNTK/FasterRCNN/RunTest.sh
+++ Nuclei-CNTK/FasterRCNN/RunTest.sh
@@ -12,15 +12,15 @@ mail -s 'Starting'  peter.gronborg.7@gmail.com <<< "Start testrun"
 #time CUDA_VISIBLE_DEVICES=3 python3 run_faster_rcnn_VGG16.py Layers/Last_relu6 pool2 relu6 > Output/Layers/Last_Relu6Out.txt 2> Output/Layers/Last_Relu6Err.txt &
 #time CUDA_VISIBLE_DEVICES=0 python3 run_faster_rcnn_VGG16.py Ref/Ref_1 > Output/Ref/Ref_1Out.txt 2> Output/Ref/Ref_1Err.txt &
 #time CUDA_VISIBLE_DEVICES=2 python3 run_faster_rcnn_VGG16.py Ref/Ref_2 > Output/Ref/Ref_2Out.txt 2> Output/Ref/Ref_2Err.txt &
-time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_01/0_01 0.01 > Output/SigmaCenter/Center_01/Out/0_01.txt 2> Output/SigmaCenter/Center_01/Err/0_01.txt &
+time CUDA_VISIBLE_DEVICES=6 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_1/0_01 0.01 > Output/SigmaCenter/Center_1/Out/0_01.txt 2> Output/SigmaCenter/Center_1/Err/0_01.txt &
 wait
-time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_01/0_1 0.1 > Output/SigmaCenter/Center_01/Out/0_1.txt 2> Output/SigmaCenter/Center_01/Err/0_1.txt &
+time CUDA_VISIBLE_DEVICES=6 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_1/0_1 0.1 > Output/SigmaCenter/Center_1/Out/0_1.txt 2> Output/SigmaCenter/Center_1/Err/0_1.txt &
 wait
-time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_01/1 1 > Output/SigmaCenter/Center_01/Out/1.txt 2> Output/SigmaCenter/Center_01/Err/1.txt &
+time CUDA_VISIBLE_DEVICES=6 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_1/1 1 > Output/SigmaCenter/Center_1/Out/1.txt 2> Output/SigmaCenter/Center_1/Err/1.txt &
 wait
-time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_01/10 10 > Output/SigmaCenter/Center_01/Out/10.txt 2> Output/SigmaCenter/Center_01/Err/10.txt &
+time CUDA_VISIBLE_DEVICES=6 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_1/10 10 > Output/SigmaCenter/Center_1/Out/10.txt 2> Output/SigmaCenter/Center_1/Err/10.txt &
 wait
-time CUDA_VISIBLE_DEVICES=5 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_01/100 100 > Output/SigmaCenter/Center_01/Out/100.txt 2> Output/SigmaCenter/Center_01/Err/100.txt &
+time CUDA_VISIBLE_DEVICES=6 python3 run_faster_rcnn_VGG16.py SigmaCenter/Center_1/100 100 > Output/SigmaCenter/Center_1/Out/100.txt 2> Output/SigmaCenter/Center_1/Err/100.txt &
 
 
 wait
